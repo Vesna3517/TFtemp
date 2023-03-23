e98d12b57c66b1e4fe3c3e2ebc2fb9ad0583a851
Sergey M <vesna3517@gmail.com>
2023-03-23 09:43:02 +0300

    1

--- config.tf
+++ config.tf
@@ -20,3 +20,14 @@ resource "aws_instance" "build" {
     Name = "build"
   }
 }
+
+resource "aws_instance" "web" {
+  ami           = "ami-0e067cc8a2b58de59"
+  instance_type = "t2.micro"
+  key_name      = "depkey"
+  vpc_security_group_ids = ["sg-09497a8983bc03a7a"]
+
+  tags = {
+    Name = "web"
+  }
+}
